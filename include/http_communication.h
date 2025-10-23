6c0b9728e1940fccd8fb68d21b2ce365abd89716
Dmytro Kovalchuk <dmytro.kovalchuk@plvision.eu>
2025-10-23 18:25:02 +0300

    Change Request and Response structs for better accuracy of real HTTP packet
    Signed-off-by: Dmytro Kovalchuk <dmytro.kovalchuk@plvision.eu>

--- include/http_communication.h
+++ include/http_communication.h
@@ -1,22 +1,27 @@
 #ifndef HTTP_COMMUNICATION_H
 #define HTTP_COMMUNICATION_H
 
+typedef unsigned long size_t;
+
 struct Request {
-    char method[8];
-    char path[256];
-    char version[16];
-    char headers[512];
-    char body[2048];
+    char method[16];
+    char path[512];
+    char version[32];
+    char headers[8192];
+    char* body;
+    size_t body_size;
 };
 
 struct Response {
     char status[64];
-    char body[1024];
+    char headers[256];
+    char* body;
+    size_t body_size;
 };
 
 struct Request parse_request(const char* raw_request);
 
-char* create_response(int client_socket, struct Request request);
+char* create_response(struct Request request);
 
 size_t parse_content_length(const char* headers);
 
